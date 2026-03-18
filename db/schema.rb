4a8bdd40ad772448a24fc9ecefb836538ece2030
katotetsu <tech@katotetsu.net>
2026-03-18 14:41:29 +0900

    add:scaffordの学習

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,22 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2026_03_18_022729) do
+ActiveRecord::Schema[8.1].define(version: 2026_03_18_045001) do
   create_table "users", force: :cascade do |t|
     t.integer "age"
     t.datetime "created_at", null: false
     t.string "name"
     t.datetime "updated_at", null: false
   end
+
+  create_table "weight_histories", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "memo"
+    t.datetime "updated_at", null: false
+    t.integer "user_id", null: false
+    t.integer "weight"
+    t.index ["user_id"], name: "index_weight_histories_on_user_id"
+  end
+
+  add_foreign_key "weight_histories", "users"
 end
