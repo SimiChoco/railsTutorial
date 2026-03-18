70c49053f74095b88113be09e1a1b3843a30adda
katotetsu <tech@katotetsu.net>
2026-03-18 13:43:25 +0900

    fix:routesの学習

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get '/', to: 'top#index'
+  get '/users/:id', to: 'users#show', as: "user"
+  get '/users', to: 'users#index', as: "users"
 
   # Render dynamic PWA files from app/views/pwa/* (remember to link manifest in application.html.erb)
   # get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
