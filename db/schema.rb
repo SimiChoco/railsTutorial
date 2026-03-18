6048eac8faab5a8896948da34df85d09fac61c8f
katotetsu <tech@katotetsu.net>
2026-03-18 15:33:47 +0900

    add:seedsの学習

--- db/schema.rb
+++ db/schema.rb
@@ -14,8 +14,9 @@ ActiveRecord::Schema[8.1].define(version: 2026_03_18_045001) do
   create_table "users", force: :cascade do |t|
     t.integer "age"
     t.datetime "created_at", null: false
-    t.string "name"
+    t.string "name", limit: 128, null: false
     t.datetime "updated_at", null: false
+    t.index ["name"], name: "index_users_on_name"
   end
 
   create_table "weight_histories", force: :cascade do |t|
