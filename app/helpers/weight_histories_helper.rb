943c5e54c9d6ef946471bf11f0dbf086750e1755
katotetsu <tech@katotetsu.net>
2026-03-18 15:11:41 +0900

    add:helper学習

--- app/helpers/weight_histories_helper.rb
+++ app/helpers/weight_histories_helper.rb
@@ -1,2 +1,11 @@
 module WeightHistoriesHelper
+    def weight_comment(weight)
+        if weight <50
+            "#{weight}kgは軽すぎます"
+        elsif weight <70
+            "#{weight}kgは標準です"
+        else
+            "#{weight}kgは重すぎます"
+        end
+    end
 end
--- app/models/weight_history.rb
+++ app/models/weight_history.rb
@@ -1,3 +1,23 @@
 class WeightHistory < ApplicationRecord
   belongs_to :user
+  before_validation :set_memo
+
+  validates :user_id, presence: { message: "ユーザーIDを入力してください" }
+  validates :memo,length:{ maximum: 255, too_long: "メモは255文字以内で入力してください" }
+  validate :weight_validate
+
+
+  def weight_validate
+    if weight < 1
+      errors.add(:weight, "体重を正しく入力してください")
+    end
+  end
+  
+  def set_memo
+    if memo.nil?
+      #代入するときはself.が必要
+      self.memo = "なし"
+    end
+  end
+
 end
